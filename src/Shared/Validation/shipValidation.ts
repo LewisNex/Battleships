23cb30da5d977fa57bc544cb638ba4cc2a9d2f7a
LewisNex <lewislightnex@gmail.com>
2021-02-25 23:21:25 +0000

    TDD some ship validation

--- src/Shared/Validation/shipValidation.ts
+++ src/Shared/Validation/shipValidation.ts
@@ -1,5 +1,30 @@
+import { Coordinate } from '../Models/coordinates';
 import { Ship } from "../Models/ships";
 
-export const isShipValid = (ship: Ship): boolean => {
-	return ship.Coordinates[0].row === 1;
-}
+export const isShipValid = (ship: Ship): boolean =>
+	areCoordinatesInline(...ship.Coordinates)
+	&& isCoordinateChainAdjacent(...ship.Coordinates)
+	&& areCoordinatesUnique(...ship.Coordinates);
+
+const areCoordinatesUnique = (...coords: Coordinate[]): boolean =>
+	coords.map((x) => `${x.column}${x.row}`)
+		.every((x, i, a) => a.lastIndexOf(x) === i)
+
+const areCoordinatesHorizontal = (...coords: Coordinate[]): boolean =>
+	coords.every(coord => coords[0].row === coord.row);
+
+const areCoordinatesVertical = (...coords: Coordinate[]): boolean => 
+	coords.every(coord => coords[0].column === coord.column);
+
+const areCoordinatesInline = (...coords: Coordinate[]): boolean =>
+	areCoordinatesHorizontal(...coords) || areCoordinatesVertical(...coords);
+
+const isCoordinatePairAdjacent = (coord1: Coordinate, coord2: Coordinate): boolean => 
+	Math.abs(coord1.row - coord2.row) === 1 
+	|| Math.abs(coord1.column.charCodeAt(0) - coord2.column.charCodeAt(0)) === 1;
+
+const isCoordinateChainAdjacent = (...coords: Coordinate[]): boolean =>
+	coords.map((x, i) => [x, coords[i+1]] as const)
+		  .filter(([_, y]) => y !== undefined)
+		  .every(([x, y]) => isCoordinatePairAdjacent(x, y))
+	
--- src/__tests__/Shared/Validation/shipValidation.ts
+++ src/__tests__/Shared/Validation/shipValidation.ts
@@ -1,23 +1,70 @@
+import { FixedLengthArray } from './../../../Shared/Models/ships';
+import { Coordinate } from './../../../Shared/Models/coordinates';
 import { Ship, ShipType } from "../../../Shared/Models/ships"
 import { isShipValid } from "../../../Shared/Validation/shipValidation"
 
 describe('Shared/Validation/shipValidation', () => {
-	test('isShipValid return true for valid', () => {
-		let ship: Ship = {
-			Coordinates: [{row: 1, column:'A'},
-						  {row: 1, column:'B'}],
-			ReloadRate: 1,
-			Type: ShipType.Destroyer
-		}
-		expect(isShipValid(ship)).toBe(true);
+	test('isShipValid return false for Destroyer coords ontop of eachother', () => 
+		expectIsDestroyerValid(
+			[{row: 1, column:'A'},
+			 {row: 1, column:'A'}]).toBe(false));
+	test('isShipValid return false for Destroyer coords too far apart vertically', () => 
+		expectIsDestroyerValid(
+			[{row: 1, column:'A'},
+			 {row: 1, column:'D'}]).toBe(false));
+	test('isShipValid return false for Destroyer coords too far apart horizontally', () =>
+		expectIsDestroyerValid(
+			[{row: 1, column:'A'},
+			 {row: 3, column:'A'}]).toBe(false));
+	test('isShipValid return false for Destroyer coords too far apart horizontally and vertically', () =>
+		expectIsDestroyerValid(
+			[{row: 1, column:'A'},
+			 {row: 3, column:'E'}]).toBe(false));
+	test('isShipValid return false for Destroyer coords backslash-diagonal', () =>
+		expectIsDestroyerValid(
+			[{row: 1, column:'A'},
+			 {row: 2, column:'B'}]).toBe(false));
+	test('isShipValid return false for Destroyer coords forwardslash-diagonal', () => {
+		expectIsDestroyerValid(
+			[{row: 5, column:'C'},
+			 {row: 4, column:'B'}]).toBe(false);
 	});
-	test('isShipValid return false for invalid', () => {
-		let ship: Ship = {
-			Coordinates: [{row: 2, column:'A'},
-						  {row: 2, column:'A'}],
-			ReloadRate: 1,
-			Type: ShipType.Destroyer
-		}
-		expect(isShipValid(ship)).toBe(false);
+	test('isShipValid return true for valid horizontal Destroyer coords', () => {
+		expectIsDestroyerValid(
+			[{row: 5, column:'G'},
+			 {row: 5, column:'F'}]).toBe(true);
 	});
+	test('isShipValid return true for valid vertical Destroyer coords', () => {
+		expectIsDestroyerValid(
+			[{row: 9, column:'I'},
+			 {row: 10, column:'I'}]).toBe(true);
+	});
+
+	test('isShipValid return false for chained adjacent horizontal Carrier coords over 2 coordinates', () => {
+		expectIsCarrierValid(
+			[{row: 10, column:'E'},
+			 {row: 10, column:'F'},
+			 {row: 10, column:'G'},
+			 {row: 10, column:'H'},
+			 {row: 10, column:'G'}]).toBe(false);
+	});
+
 });
+
+// Helpers:
+const expectIsDestroyerValid = (coords: FixedLengthArray<Coordinate, 2>) => {
+	let ship: Ship = {
+		Coordinates: coords,
+		ReloadRate: 1,
+		Type: ShipType.Destroyer
+	}
+	return expect(isShipValid(ship));
+}
+const expectIsCarrierValid = (coords: FixedLengthArray<Coordinate, 5>) => {
+	let ship: Ship = {
+		Coordinates: coords,
+		ReloadRate: 1,
+		Type: ShipType.Carrier
+	}
+	return expect(isShipValid(ship));
+}
