b335ac385331d0b9abfbe6b60a1dc1f03a81722c
LewisNex <lewislightnex@gmail.com>
2021-04-22 20:57:12 +0100

    BS006 Fix debugging with Jest.

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,10 @@ module.exports = {
   ],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  collectCoverage: true,
+  mapCoverage: true,
 };
